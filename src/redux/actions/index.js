77a50b75da5572f9a754949fde52e60a74c96b6e
Willian Dutra <contatowilliandutra@gmail.com>
2022-10-31 15:33:58 -0300

    Configuração inicial da Store

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,17 @@
 // Coloque aqui suas actions
+export const USER_LOGIN = 'USER_LOGIN';
+export const REQUEST_API = 'REQUEST_API';
+export const RESPONSE_API = 'RESPONSE_API';
+
+export const userLogin = (email) => ({
+  type: USER_LOGIN,
+  payload: email,
+});
+
+// const requestStarted = () => ({
+//   type: REQUEST_API,
+// });
+
+export const fetchCoins = () => ({
+  type: RESPONSE_API,
+});
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { REQUEST_API, RESPONSE_API } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
+
+const wallet = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case REQUEST_API:
+    return { ...state, isLoading: true };
+  case RESPONSE_API:
+    return { ...state, isLoading: false, payload };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
