ef382c6afe96af731a0526ee1a248262e6bb9ae0
Willian Dutra <contatowilliandutra@gmail.com>
2022-10-31 16:38:39 -0300

    Requisito 1 - Criando página de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,87 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userLogin } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+  }
+
+  enableButton = () => {
+    const { email, password } = this.state;
+    const emailValidation = email.includes('@') && email.includes('.com');
+
+    const minLength = 6;
+    const passwordValidation = password.length >= minLength;
+
+    if (emailValidation && passwordValidation) {
+      this.setState(() => ({ isDisabled: false }));
+    } else {
+      this.setState(() => ({ isDisabled: true }));
+    }
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState(() => ({ [name]: value }), this.enableButton);
+  };
+
+  handleSubmit = () => {
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(userLogin(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, isDisabled, password } = this.state;
+    return (
+      <div>
+        <label htmlFor="email-input">
+          Email
+          <input
+            id="email-input"
+            name="email"
+            type="text"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Password
+          <input
+            id="password-input"
+            name="password"
+            type="password"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ isDisabled }
+          onClick={ this.handleSubmit }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
